e2b3fd134bad28ac0bf22f320edcb6fbbc3c6cee
unknown <girl.climging@gmail.com>
2024-05-30 05:32:01 +0100

    Implemented abs, int and round

--- calculator.py
+++ calculator.py
@@ -13,41 +13,77 @@ def read_number(line, index):
     token = {'type': 'NUMBER', 'number': number}
     return token, index
 
-def read_open(line, index):
-    token = {'type': 'OPEN'}
-    return token, index + 1
+def read_abs(line, index):
+    if (line[index + 1] == 'b' and
+        line[index + 2] == 's' and
+        line[index + 3] == '('):
+        return {'type': 'ABS'}, index + 4
 
-def read_close(line, index):
-    token = {'type': 'CLOSE'}
-    return token, index + 1
+def read_int(line, index):
+    if (line[index + 1] == 'n' and
+        line[index + 2] == 't' and
+        line[index + 3] == '('):
+        return {'type': 'INT'}, index + 4
 
-def read_ops(line, index):
+def read_round(line, index):
+    if (line[index + 1] == 'o' and
+        line[index + 2] == 'u' and
+        line[index + 3] == 'n' and
+        line[index + 4] == 'd' and
+        line[index + 5] == '('):
+        return {'type': 'ROUND'}, index + 6
+
+def read_ops(letter):
     ops = {'+': 'PLUS', '-': 'MINUS', '*': 'MULTIPLY', '/': 'DIVIDE'}
-    op = ops.get(line[index])
+    op = ops.get(letter)
     if not op:
         print("Invalid character found: ' + line[index]")
         exit(1)
-    return {'type': op}, index + 1
+    return {'type': op}
 
 def tokenize(line):
     tokens = []
     index = 0
     bracket_cnt = 0
+    abs_cnt = 0
+    int_cnt = 0
+    round_cnt = 0
     while index < len(line):
         if line[index].isdigit():
             (token, index) = read_number(line, index)
-        elif line[index] == '(':
-            (token, index) = read_open(line, index)
+        elif line[index] == 'a':
+            (token, index) = read_abs(line, index)
             bracket_cnt += 1
-            bracket_cnt
+            abs_cnt = bracket_cnt
+        elif line[index] == 'i':
+            (token, index) = read_int(line, index)
+            bracket_cnt += 1
+            int_cnt = bracket_cnt
+        elif line[index] == 'r':
+            (token, index) = read_round(line, index)
+            bracket_cnt += 1
+            round_cnt = bracket_cnt
+        elif line[index] == '(':
+            token = {'type': 'OPEN'}
+            index += 1
+            bracket_cnt += 1
         elif line[index] == ')':
             if bracket_cnt == 0:
                 print('Unmatched ) found')
                 exit(1)
-            (token, index) = read_close(line, index)
+            if bracket_cnt == abs_cnt:
+                token = {'type': 'ABS_CLOSE'}
+            elif bracket_cnt == int_cnt:
+                token = {'type': 'INT_CLOSE'}
+            elif bracket_cnt == round_cnt:
+                token = {'type': 'ROUND_CLOSE'}
+            else:
+                token = {'type': 'CLOSE'}
+            index += 1
             bracket_cnt += -1
         else:
-            (token, index) = read_ops(line, index)
+            token = read_ops(line[index])
+            index += 1
         tokens.append(token)
     if bracket_cnt > 0:
         print('Unmatched ( found')
@@ -64,8 +100,10 @@ def evaluate(tokens):
                 if op_stack:
                     if op_stack[-1] == 'MULTIPLY':
                         num_stack.append(safePop(num_stack) * token['number'])
+                        op_stack.pop()
                     elif op_stack[-1] == 'DIVIDE':
                         num_stack.append(safePop(num_stack) / token['number'])
+                        op_stack.pop()
                     else:
                         num_stack.append(token['number'])
                 else:
@@ -74,8 +112,25 @@ def evaluate(tokens):
             case 'CLOSE':
                 while op_stack[-1] != 'OPEN':
                     calculate(num_stack, op_stack)
+                op_stack.pop()
+            case 'ABS_CLOSE':
+                while op_stack[-1] != 'ABS':
+                    calculate(num_stack, op_stack)
+                op_stack.pop()
+                num_stack.append(abs(num_stack.pop()))
+            case 'INT_CLOSE':
+                while op_stack[-1] != 'INT':
+                    calculate(num_stack, op_stack)
+                op_stack.pop()
+                num_stack.append(int(num_stack.pop()))
+            case 'ROUND_CLOSE':
+                while op_stack[-1] != 'ROUND':
+                    calculate(num_stack, op_stack)
+                op_stack.pop()
+                num_stack.append(round(num_stack.pop()))
             case default:
-                if op_flag:
+                if (op_flag and
+                    default in {'PLUS', 'MINUS', 'MULTIPLY', 'DIVIDE'}):
                     invalid()
                 op_stack.append(default)
                 op_flag = True
@@ -91,6 +146,10 @@ def calculate(num_stack, op_stack):
             num_stack.append(safePop(num_stack) + safePop(num_stack))
         case 'MINUS':
             num_stack.append(- safePop(num_stack) + safePop(num_stack))
+        case 'MULTIPLY':
+            num_stack.append(safePop(num_stack) * safePop(num_stack))
+        case 'DIVIDE':
+            num_stack.append(1 / safePop(num_stack) * safePop(num_stack))
 
 def safePop(stack):
     if not stack:
@@ -100,20 +159,3 @@ def safePop(stack):
 def invalid():
     print('Invalid syntax')
     exit(1)
-
-# def evaluate(tokens):
-#     answer = 0
-#     tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
-#     index = 1
-#     stack = []
-#     while index < len(tokens):
-#         if tokens[index]['type'] == 'NUMBER':
-#             if tokens[index - 1]['type'] == 'PLUS':
-#                 answer += tokens[index]['number']
-#             elif tokens[index - 1]['type'] == 'MINUS':
-#                 answer -= tokens[index]['number']
-#             else:
-#                 print('Invalid syntax')
-#                 exit(1)
-#         index += 1
-#     return answer
--- calculator_test.py
+++ calculator_test.py
@@ -9,11 +9,32 @@ def test(line):
     else:
         print("FAIL! (%s should be %f but was %f)" % (line, expected_answer, actual_answer))
 
-# Add more tests to this function :)
 def run_test():
     print("==== Test started! ====")
+
     test("1+2")
     test("1.0+2.1-3")
+    test("1.4+2*3.1")
+    test("1.5/3.2+2")
+
+    test("(1.2+1.3)*3")
+    test("(2)+(3)")
+    test("2*(3+4*(2+5))")
+    test("((1+2)*3+4)*5")
+
+    test("abs(5)")
+    test("abs(2-3)")
+    test("abs(1.1*(3.5-7))")
+    test("int(2.5)")
+    test("int(2-3)")
+    test("int(1.2*(3.4+1))")
+    test("round(3.1)")
+    test("round(6+2)")
+    test("round((2.3+1.2)*3)")
+
+    test("abs(int(round(3.2-5.9)+1.5)+2.6)+7.2")
+    test("round(2.8)+int(3.1+(1.2*abs(3.8-12)))")
+
     print("==== Test finished! ====\n")
 
 run_test()
